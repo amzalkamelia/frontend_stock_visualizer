7e06a0188e1567880276bc6577360de80e44b8b0
amzalkamelia <67685301+amzalkamelia@users.noreply.github.com>
2024-01-16 22:42:39 +0100

    Add files via upload
    Initialisation

---
+++ Frontend_stock_visualizer-2/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+
+import { Routes } from '@angular/router';
+import { YourComponent } from './your.component';
+
+export const routes: Routes = [
+  { path: 'your-path', component: YourComponent }
+];
