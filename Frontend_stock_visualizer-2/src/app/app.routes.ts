c284b9c8f758b1cc712e21d0711b6f955a0d35ad
amzalkamelia <67685301+amzalkamelia@users.noreply.github.com>
2024-01-17 00:39:23 +0100

    test

--- Frontend_stock_visualizer-2/src/app/app.routes.ts
+++ Frontend_stock_visualizer-2/src/app/app.routes.ts
@@ -1,7 +1,6 @@
-
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 export const routes: Routes = [
-  { path: 'app', component: AppComponent }
+  { path: 'test', component: TestComponent }
 ];
